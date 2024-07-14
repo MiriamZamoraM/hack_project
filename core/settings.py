bcc64894c6552b4939fc7e7ef4287f4877179599
miriam_zm <98359783+MiriamZamoraM@users.noreply.github.com>
2024-07-13 21:21:05 -0600

    update proyecto

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 from decouple import config
+from decouple import Csv  # noqa: F401
 
 ENV = config("ENV", default="dev")
 
@@ -108,16 +109,25 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.NumericPasswordValidator'
+        ),
     },
 ]
 
