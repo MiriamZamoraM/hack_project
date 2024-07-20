ccea300238cebf047c308f3312739262f62d5820
miriam_zm <98359783+MiriamZamoraM@users.noreply.github.com>
2024-07-20 14:33:59 -0600

    users comp 9

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,7 +5,7 @@ from .views import RefreshTokenView
 from .views import RegistryView
 
 urlpatterns = [
-    path(route="registry/", view=RegistryView.as_view(), name="registry"),
     path(route="login/", view=LoginView.as_view(), name="login"),
     path(route="refreshToken/", view=RefreshTokenView.as_view(), name="refresh_token"),
+    path(route="registry/", view=RegistryView.as_view(), name="registry"),
 ]
